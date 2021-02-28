3f241043ef7bb2b2ed4d5f0293c6b27c266e6a53
SP <74065235+SPchalil@users.noreply.github.com>
2021-02-28 01:34:38 -0330

    added files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './Avatar.png';
+import './App.css';
+import List from './List.js'
+
+class App extends React.Component {
+  render (){
+    return(
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          MY To-Do App
+        </h1>
+        <List/>
+      </header>
+    </div>
+
+    );
+
+  }
+  
+}
+export default App;
