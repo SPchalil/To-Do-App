b51b0e136ba19938e2c6514cc6ea4090ee79e1c6
SP <74065235+SPchalil@users.noreply.github.com>
2021-02-28 10:52:47 -0330

    Added logic

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import './App.css';
 import List from './List.js'
 
 class App extends React.Component {
-  render (){
-    return(
+  
+  constructor(props){
+    super(props);
+}
+  
+  render() {
+    return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          -- My To-Do App ✍--
-        </h1>
-        <List/>
-      </header>
-    </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className = "heading">
+            My To-Do List ✍
+          </h1>
+          <List />
+        </header>
+      </div>
 
     );
 
   }
-  
+
 }
+
 export default App;
--- src/List.js
+++ src/List.js
@@ -2,28 +2,92 @@ import React from 'react';
 import './App.css';
 
 class List extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+          newItem: "",
+          list : []
+        }
+    
+    }
+  
+    addItem(todoValue){
+      if(todoValue !== ""){
+        const newItem = {
+          id: Date.now(),
+          value:todoValue,
+          isDone:false
+        };
+        const list = [...this.state.list];
+        list.push(newItem);
+        this.setState({
+          list,
+          newItem:""
+        });
+      }
+    }
+  
+    deleteItem(id){
+       const list = [...this.state.list] ;
+       const updatedlist = list.filter(item => item.id !==id);
+       this.setState({list:updatedlist})
+    }
+
+    updateInput(input){
+        this.setState({newItem:input});
+    }
+
     render() {
         return (
             <div className="container">
-                Add an Item.....
-                <br />
+                <p>... Add an Item .....</p>
                 <input
                     type="text"
                     className="input-text"
                     placeholder="Write a To-do"
+                    required
+                    value = {this.state.newItem}
+                    onChange = {e=>this.updateInput(e.target.value)}
                 />
                 <button
-                    className="add-btn" > Add To-do
-            </button>
-                <div className="list">
-                    <ul>
+                    className="add-btn" 
+                    onClick = {()=>this.addItem(this.state.newItem)}
+                    disabled = {!this.state.newItem.length}
+                    > ADD TO-DO
+                </button>
+                <div className="list-container">
+                    <ul className="list">
+                    
+                        {this.state.list.map(item => {
+                            return(
+                                
+                                <li key = {item.id}>
+                                    <input
+                                    type = "checkbox" 
+                                    name= "idDone"
+                                    checked = {item.isDone}
+                                    onChange={() =>{}}
+                                    
+                                    />
+                                    {item.value}
+                                    <button
+                                        className = "delete-btn"
+                                        onClick = {() => this.deleteItem(item.id)}
+                                        >
+                                        DELETE
+                                    </button>
+                                </li>
+                                
+                            );
+                        })}
                         <li>
-                            <input type="checkbox" name="" id="" />
-                        Record youtube videos
-                        <button className="delete-btn">Delete</button>
+                        <input type="checkbox" name="" id="" />
+                        Review codes
+                        <button className="delete-btn">DELETE</button>
 
 
-                        </li>
+                        </li>   
                     </ul>
 
                 </div>
